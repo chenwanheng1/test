e60c953e99a8ebdd3139799a2dea6e9e245769e0
chenwanheng <chenwanheng@126.com>
2023-11-30 23:21:39 +0800

    commit test

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+//
+// Created by drichyn on 2023/11/30.
+//
+
+#include "hello.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!";
+    return 0;
+}
---
+++ hello.h
@@ -0,0 +1,13 @@
+//
+// Created by drichyn on 2023/11/30.
+//
+
+#ifndef TEST_HELLO_H
+#define TEST_HELLO_H
+
+
+class hello {
+};
+
+
+#endif //TEST_HELLO_H
